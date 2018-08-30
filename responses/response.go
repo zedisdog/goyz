eb899c818dc7727daa094c99a342263d2d5c1028
zed <zedsmail@163.com>
2018-08-30 16:09:57 +0800

    数据结构完善

--- responses/response.go
+++ responses/response.go
@@ -1,18 +1,20 @@
 package responses
 
+type Account struct {
+	Seller          string `json:"seller"`
+	FromBuyerMobile string `json:"from_buyer_mobile"`
+	Money           string `json:"money"`
+	Mobile          string `json:"mobile"`
+	Nickname        string `json:"nickname"`
+	CreateAt        string `json:"create_at"`
+	OrderNum        int    `json:"order_num"`
+	FansId          int    `json:"fans_id"`
+}
+
 type AccountsResponse struct {
 	Response struct {
-		Accounts []struct {
-			Seller          string `json:"seller"`
-			FromBuyerMobile string `json:"from_buyer_mobile"`
-			Money           string `json:"money"`
-			Mobile          string `json:"mobile"`
-			Nickname        string `json:"nickname"`
-			CreateAt        string `json:"create_at"`
-			OrderNum        int    `json:"order_num"`
-			FansId          int    `json:"fans_id"`
-		} `json:"accounts"`
-		TotalResults int `json:"total_results"`
+		Accounts     []Account `json:"accounts"`
+		TotalResults int       `json:"total_results"`
 	} `json:"response"`
 }
 
